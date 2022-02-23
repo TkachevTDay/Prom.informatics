2831a2927155a130cd01bcdc3ab0d2f558b42980
Matvey Gostev <matgost@yandex.ru>
2022-02-23 22:17:58 +0300

    Add database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Project(models.Model):
+    path_link = models.CharField(max_length=255)
+    upload_date = models.IntegerField()
+    last_open_date = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    mark = models.CharField(max_length=10)
+
+
+class Docker_ISO(models.Model):
+    project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    link_to_iso = models.CharField(max_length=255)
