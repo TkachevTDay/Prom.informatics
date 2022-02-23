b7af13d0d6de8f236593745265167f7d6f81b673
Matvey Gostev <matgost@yandex.ru>
2022-02-23 21:24:50 +0300

    Add empty index.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index_page(request):
+    context = {}
+    return render(request, 'index.html', context)
--- prominformatics/urls.py
+++ prominformatics/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main.views.index_page),
 ]
