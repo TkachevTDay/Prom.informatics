7074af3b20c75148ad52ed6ccf5dd0ac1f5d6ee6
Matvey Gostev <matgost@yandex.ru>
2022-03-26 14:45:20 +0300

    DB changes + br goes not br

--- main/models.py
+++ main/models.py
@@ -4,16 +4,16 @@ from django.utils import timezone
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(default="", max_length=255)
-    description = models.CharField(default="", max_length=2047)
-    path_link = models.CharField(max_length=255)
+    name = models.TextField(default="", max_length=255)
+    description = models.TextField(default="", max_length=2047)
+    path_link = models.TextField(max_length=255)
     upload_date = models.IntegerField()
     last_open_date = models.DateTimeField(default=timezone.now)
-    author = models.CharField(max_length=255)
-    department = models.CharField(max_length=255)
-    mark = models.CharField(max_length=10)
+    author = models.TextField(max_length=255)
+    department = models.TextField(max_length=255)
+    mark = models.TextField(max_length=10)
 
 
 class Docker_ISO(models.Model):
     project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-    link_to_iso = models.CharField(max_length=255)
+    link_to_iso = models.TextField(max_length=255)
