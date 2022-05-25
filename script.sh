199e1caad81229e64a3250f39cda8a666f4bd82f
AulderWare <tiikeri.jo@gmail.com>
2022-05-25 21:18:56 +0300

    deleted clone.sh, auto generating clone.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 python3 manage.py makemigrations
 python3 manage.py migrate
+if [ -e $HOME/clone.sh ]
+then
+  echo "file located, ERROR"
+else
+  touch clone.sh
+fi
 python3 manage.py collectstatic --noinput
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('system', 'prominfnotification@yandex.ru', '1') if not (User.objects.filter(username='system')) else print('system user already exists') " | python manage.py shell
 daphne -b 0.0.0.0 --port 8080 prominformatics.asgi:application
