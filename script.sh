2247928eab5c80d298e0219ad31b70e5c53b1c73
Matvey Gostev <matgost@yandex.ru>
2022-05-28 06:30:28 +0300

    Fixes

--- script.sh
+++ script.sh
@@ -7,20 +7,6 @@ then
 else
   touch clone.sh
 fi
-if [ -e $HOME/nginx_dynamically_build_files/nginx.conf ]
-then
-  echo "file located, ERROR"
-else
-  touch /prominf/nginx_dynamically_build_files/nginx.conf
-  chmod 666 /prominf/nginx_dynamically_build_files/nginx.conf
-fi
-if [ -e $HOME/nginx_dynamically_build_files/nginx_build.sh ]
-then
-  echo "file located, ERROR"
-else
-  touch /prominf/nginx_dynamically_build_files/nginx_build.sh
-  chmod 666 /prominf/nginx_dynamically_build_files/nginx_build.sh
-fi
 python3 manage.py collectstatic --noinput
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('system', 'prominfnotification@yandex.ru', '1') if not (User.objects.filter(username='system')) else print('system user already exists') " | python manage.py shell
 daphne -b 0.0.0.0 --port 8080 prominformatics.asgi:application
